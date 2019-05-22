3c1660a8eafc3b219d5bac201a16ebd302ad4a1b
Chui <cchu0415@corp.sysco.com>
2019-05-22 13:29:51 -0500

    changed port configuration and add @csstools/normalize.css

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-app.listen(3001, () => console.log('Express server is running on localhost: 3001'))
+const port = 3999
+app.listen(port, () => console.log(`Express server is running on localhost: ${port}`))
 //=============================================================
 let servers = [1, 2, 3];
 app.get('/getFiles', (req, res) => {
