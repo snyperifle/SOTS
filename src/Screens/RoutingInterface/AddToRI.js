9e8a0873a61c9059e12633b21e87d098462fa11e
Calvin <calvinchui382025@yahoo.com>
2019-06-10 13:43:31 -0500

    test

--- src/Screens/RoutingInterface/AddToRI.js
+++ src/Screens/RoutingInterface/AddToRI.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { Redirect } from "react-router-dom";
 //=============================================================
+import { Container, Row, Col } from 'react-bootstrap';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
@@ -33,12 +34,10 @@ class AddToRI extends React.Component {
       selectedOpCoRouters: this.props.file.filter((item) => item.split(' ')[1].includes(String(this.state.selectedOpCo.num)))
     })
     temp = temp.join("\r\n")
-    // console.log(temp);
     axios.post('/updateUserConfigs', {
       data: temp
     })
       .then((response) => {
-        // console.log(response.data);
         this.props.updateOpCo();
         this.setState({ responseMessage: response.data })
       })
@@ -50,87 +49,94 @@ class AddToRI extends React.Component {
   render() {
     if (this.props.file.length === 0) return <Redirect to='/' />
     return (
-      <div>
-        <h2>Add {this.props.userId ? this.props.userId : 'user'} to Routing Interface</h2>
-        {this.props.userId === '' ? <h2 style={{ color: 'red' }}>Please enter a User ID</h2> : null}
-        {/* //============================================================= */}
-        <form
-          autoComplete="off"
-          style={{ marginBottom: 5 }}
-        >
-          <FormControl>
-            <InputLabel >Select OpCo</InputLabel>
-            <Select
-              style={{ width: 250 }}
-              value={this.state.selectedOpCo || "No Opco Selected"}
-              onChange={(event) => {
-                this.setState({
-                  selectedOpCo: event.target.value,
-                  selectedOpCoRouters: this.props.file.filter((item) => item.split(' ')[1].includes(String(event.target.value.num)))
-                })
-              }}
+      <Container className="MainPage">
+        <Row className="Title">
+          <Col style={{ alignSelf: 'center' }}>
+            <h2>Add {this.props.userId ? this.props.userId : 'user'} to Routing Interface</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {this.props.userId === '' ? <h2 style={{ color: 'red' }}>Please enter a User ID</h2> : null}
+            {/* //============================================================= */}
+            <form
+              autoComplete="off"
+              style={{ marginBottom: 5 }}
             >
-              {this.props.allOpCo.map((item, i) => (
-                <MenuItem key={item.num} value={item}>{item.num} - {item.name}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </form>
-        {/* //============================================================= */}
-        <form
-          autoComplete="off"
-          style={{ marginBottom: 5 }}
-        >
-          <FormControl>
-            <InputLabel >Select Router Number</InputLabel>
-            <Select
-              style={{ width: 250 }}
-              value={this.state.selectedRouterNumber || "No Device Type Selected"}
-              onChange={(event) => {
-                this.setState({
-                  selectedRouterNumber: event.target.value
-                })
-              }}
+              <FormControl>
+                <InputLabel >Select OpCo</InputLabel>
+                <Select
+                  style={{ width: 250 }}
+                  value={this.state.selectedOpCo || "No Opco Selected"}
+                  onChange={(event) => {
+                    this.setState({
+                      selectedOpCo: event.target.value,
+                      selectedOpCoRouters: this.props.file.filter((item) => item.split(' ')[1].includes(String(event.target.value.num)))
+                    })
+                  }}
+                >
+                  {this.props.allOpCo.map((item, i) => (
+                    <MenuItem key={item.num} value={item}>{item.num} - {item.name}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </form>
+            {/* //============================================================= */}
+            <form
+              autoComplete="off"
+              style={{ marginBottom: 5 }}
             >
-              {this.state.routerNumbers.map((item, i) => (
-                <MenuItem key={item[i]} value={item}>{item}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </form>
-        {/* //============================================================= */}
-        {this.state.selectedOpCo !== '' ?
-          <div>
-            <h3>Current routers from {this.state.selectedOpCo.name}</h3>
-            <ul>
-              {this.state.selectedOpCoRouters.map((item, i) => (
-                <li key={item}>
-                  {item}
-                </li>
-              ))}
-            </ul>
-          </div>
-          : null
-        }
-        {this.props.userId !== '' && this.state.selectedOpCo !== '' && this.state.selectedRouterNumber !== null ?
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              if (this.props.userId !== '' && this.state.selectedOpCo !== '' && this.state.selectedRouterNumber !== null) {
-                this.addUser();
-              }
-            }}
-          >
-            Add User
+              <FormControl>
+                <InputLabel >Select Router Number</InputLabel>
+                <Select
+                  style={{ width: 250 }}
+                  value={this.state.selectedRouterNumber || "No Device Type Selected"}
+                  onChange={(event) => {
+                    this.setState({
+                      selectedRouterNumber: event.target.value
+                    })
+                  }}
+                >
+                  {this.state.routerNumbers.map((item, i) => (
+                    <MenuItem key={item[i]} value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </form>
+            {/* //============================================================= */}
+            {this.state.selectedOpCo !== '' ?
+              <div>
+                <h3>Current routers from {this.state.selectedOpCo.name}</h3>
+                <ul>
+                  {this.state.selectedOpCoRouters.map((item, i) => (
+                    <li key={item}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              : null
+            }
+            {this.props.userId !== '' && this.state.selectedOpCo !== '' && this.state.selectedRouterNumber !== null ?
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  if (this.props.userId !== '' && this.state.selectedOpCo !== '' && this.state.selectedRouterNumber !== null) {
+                    this.addUser();
+                  }
+                }}
+              >
+                Add User
         </Button>
-          : null}
-        {this.state.responseMessage ?
-          <h5>{this.state.responseMessage}</h5> :
-          null
-        }
-        {/* //============================================================= */}
-      </div>
+              : null}
+            {this.state.responseMessage ?
+              <h5>{this.state.responseMessage}</h5> :
+              null
+            }
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
